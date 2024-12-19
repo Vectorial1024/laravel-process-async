bf764fac955f5541a113fbbe352822bc2df1e067
Vincent Wong <vincentwwy168@gmail.com>
2024-12-20 03:03:51 +0800

    Try mocking artisan file

---
+++ bootstrap/app.php
@@ -0,0 +1,18 @@
+<?php
+
+// only for testing, to support the artisan mock
+
+use Illuminate\Foundation\Application;
+use Illuminate\Foundation\Configuration\Exceptions;
+use Illuminate\Foundation\Configuration\Middleware;
+
+return Application::configure(basePath: dirname(__DIR__))
+    ->withRouting(
+        commands: __DIR__.'/../routes/console.php',
+    )
+    ->withMiddleware(function (Middleware $middleware) {
+        //
+    })
+    ->withExceptions(function (Exceptions $exceptions) {
+        //
+    })->create();
