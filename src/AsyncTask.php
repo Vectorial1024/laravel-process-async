95290f58483dac7d88af5cf38a09ddb3129436fe
Vincent Wong <vincentwwy168@gmail.com>
2025-01-01 02:31:58 +0800

    Revert temp-test code

--- src/AsyncTask.php
+++ src/AsyncTask.php
@@ -333,7 +333,7 @@ class AsyncTask
             $timeElapsed = microtime(true) - $this->laravelStartVal;
             // temp let runner print me the stats
             fwrite(STDERR, "microtime elapsed $timeElapsed" . PHP_EOL);
-            if ($timeElapsed + self::TIME_EPSILON >= $this->timeLimit) {
+            if ($timeElapsed >= $this->timeLimit) {
                 // yes
                 return true;
             }
@@ -342,7 +342,7 @@ class AsyncTask
             // because there will always be a small but significant delay between `timeout` start time and PHP start time.
             // in this case, we will look at the pre-determined timer PID to ask about the actual elapsed time through the kernel's proc data
             // this method should be slower than the microtime method
-            if (false && OsInfo::isUnix()) {
+            if (OsInfo::isUnix()) {
                 // get time elapsed in seconds
                 $tempOut = exec("ps -p {$this->timerProcID} -o etimes=");
                 // this must exist (we are still running!), otherwise it indicates the kernel is broken and we can go grab a chicken dinner instead
