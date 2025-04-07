1d02b18df0e96ace5777ba1032712e182a0ce7fa
Vincent Wong <vincentwwy168@gmail.com>
2025-04-07 17:00:40 +0800

    Allow configuring the secret key for proc-async

--- src/ProcessAsyncServiceProvider.php
+++ src/ProcessAsyncServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace Vectorial1024\LaravelProcessAsync;
 
+use function Opis\Closure\init;
 use Illuminate\Support\ServiceProvider;
 
 class ProcessAsyncServiceProvider extends ServiceProvider
@@ -11,7 +12,12 @@ class ProcessAsyncServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // pass
+        // read from the env file for the secret key (if exists) to verify our identity
+        $secretKey = env("PROCESS_ASYNC_SECRET_KEY");
+        if ($secretKey != null && strlen($secretKey) > 0) {
+            // we can set the secret key
+            init($secretKey);
+        }
     }
 
     /**
